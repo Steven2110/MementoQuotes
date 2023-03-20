9554ab1042253ab2b3511ce92035a140f2ac209c
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-03-20 09:37:30 +0700

    Initial Commit

---
+++ MementoQuotes/MementoQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoQuotesApp.swift
+//  MementoQuotes
+//
+//  Created by Steven Wijaya on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MementoQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
